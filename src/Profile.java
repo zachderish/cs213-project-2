ea432cae60aef25d1bcf3741bf40a767067adc6b
kenrickeagar <125171930+kenrickeagar@users.noreply.github.com>
2023-10-03 12:09:57 -0400

    Add Profile Constructor

--- src/Profile.java
+++ src/Profile.java
@@ -2,4 +2,19 @@ public class Profile implements Comparable<Profile>{
     private String fname;
     private String lname;
     private Date dob;
+
+    /**
+    * Profile Constructor
+    *@param fname, profile first name
+    *@param lname, profile last name
+    *@param dob date object representing date of birth
+    */
+
+    public Profile(String fname, String lname, Date dob){
+        this.fname = fname;
+        this.lname - lname;
+        this.dob = dob;
+    }
+
+    
 }

4762924ddd982dd2d3f18b4dfea8e99876e28ffc
kenrickeagar <125171930+kenrickeagar@users.noreply.github.com>
2023-10-03 12:36:20 -0400

    Added Inheritance Methods

--- src/CollegeChecking.java
+++ src/CollegeChecking.java
@@ -1,3 +1,18 @@
-public class CollegeChecking {
+public class CollegeChecking extends Checking {
+
+    public static final double MONTHLY_FEE = 0.00;
+    public static final double INTEREST_RATE = 0.01;
     private Campus campus; //campus code
+
+    @Override
+    public double monthlyInterest(){
+        return INTEREST_RATE;
+    }
+    
+    @Override
+    public double monthlyFee(){
+        return MONTHLY_FEE;
+    }
+
+    
 }

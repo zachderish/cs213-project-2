e05002548ebcc46987e708eb0f1b7a551b4dd26a
kenrickeagar <125171930+kenrickeagar@users.noreply.github.com>
2023-10-03 12:30:51 -0400

    Add Inheritance Methods

--- src/Checking.java
+++ src/Checking.java
@@ -1,3 +1,17 @@
-public class Checking  {
+public class Checking extends Account {
 
+  public static final double INTEREST_RATE = 0.01;
+  public static final double MONTHLY_FEE = 12.00;
+
+  
+@Override
+public double monthlyInterest(){
+  return INTEREST_RATE;
+}
+  
+@Override
+public double monthlyFee(){
+  return MONTHLY_FEE;
+}
+  
 }
